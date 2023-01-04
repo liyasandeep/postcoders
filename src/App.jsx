0d47b00210f00f5a89bf6d6e093a4a1336a2d67d
liyasandeep <liyasandeep1@gmail.com>
2023-01-04 15:01:18 +0000

    Task3: create form to search postcode

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [postcodeTextInput, setPostcodeTextInput] = useState("");
+  const [postcode, setPostcode] = useState("");
 
   const load = async () => {
     try {
-      const areaData = await getAreaData();
+      const areaData = await getAreaData(postcode);
 
       setAreas(areaData);
     } catch (error) {
@@ -18,12 +20,33 @@ function App() {
 
   useEffect(() => {
     load();
-  }, []);
+  }, [postcode]);
 
+  const handleChange = (event) => {
+    setPostcodeTextInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPostcode(postcodeTextInput);
+    setPostcodeTextInput("");
+  };
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form className="postcodeSearchForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter the outcode part of the postcode (eg: BB10 for 'BB10 2BQ)"
+          onChange={handleChange}
+          value={postcodeTextInput}
+          className="postcodeText"
+        />
+        <button className="searchBtn">Search</button>
+      </form>
+      {areas.length ? (
+        <h2>{`Areas for ${postcode}: ${areas.length}`}</h2>
+      ) : null}
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-export const getAreaData = async () => {
-  const { data } = await axios.get("https://api.zippopotam.us/GB/bb10");
+export const getAreaData = async (postcode) => {
+  console.log(postcode);
+  if (postcode) {
+    const { data } = await axios.get(
+      `https://api.zippopotam.us/GB/${postcode}`
+    );
 
-  return data.places;
+    return data.places;
+  } else {
+    return [];
+  }
 };
